6457ceafef217feca5173e338653a1d369f14e90
Sheldon Liao <sheldon.liao1015@gmail.com>
2023-05-29 18:39:38 +0800

    feat: mock数据

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ command, mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), viteMockServe({ mockPath: 'mock' })],
     resolve: {
       alias: {
         '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
